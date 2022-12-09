04a11cddca604406e4916a65b932a0c34249d8fe
Antonio Victor <Antoniovictor12@live.com>
2022-12-09 16:10:26 -0300

    feat(index): implementar busca no input

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
 import "./styles.css";
 
 import { toggleDropdownAnimation } from "./js/dropdown";
-import { setActiveOptionStyleClass } from "./js/utils/setActiveOptionStyleClass";
 import { setIcons, setTheme, swapTheme } from "./js/darkMode";
 import { CountryUI } from "./js/classes/CountryUI";
 import { Api } from "./js/classes/Api";
 import { parseCountrySchema } from "./js/utils/parseCountrySchema";
+import { debounce } from "./js/utils/debounceFn";
 
-// elements and classes
+// elements
 const dropdownButton = document.getElementById("dropdownButton");
 const dropdownOptions = document.getElementById("dropdownOptions");
 const darkModeBtn = document.getElementById("darkModeBtn");
+const searchInput = document.getElementById("searchInput");
+const searchForm = document.getElementById("searchForm");
 
+// classes
 const countryUI = new CountryUI(document.getElementById("flagsContainer"));
 const api = new Api("https://restcountries.com/v3.1/");
+
+// functions
+function populateDisplay(data) {
+  data.forEach((item) => {
+    countryUI.appendOnDisplay(parseCountrySchema(item));
+  });
+}
+
+const debounceGet = debounce(async (input) => {
+  let result = input === "" ? api.get("all") : api.get("name/" + input);
+
+  const data = await result;
+
+  if (data.length === 0) {
+    alert("Oops... we didn't find any results");
+    return;
+  }
+
+  countryUI.resetDisplay();
+  populateDisplay(data);
+}, 1000);
 
 // events
 dropdownButton.addEventListener("click", toggleDropdownAnimation);
@@ -28,27 +52,25 @@ dropdownOptions.addEventListener("click", (event) => {
   toggleDropdownAnimation();
 
   if (target.innerText === "All") {
-    api.get("all").then((data) => {
-      data.forEach((item) => {
-        countryUI.appendOnDisplay(parseCountrySchema(item));
-      });
-    });
+    api.get("all").then(populateDisplay);
     return;
   }
 
-  api.get("region/" + target.innerText).then((data) => {
-    data.forEach((item) => {
-      countryUI.appendOnDisplay(parseCountrySchema(item));
-    });
-  });
+  api.get("region/" + target.innerText).then(populateDisplay);
+});
+
+searchInput.addEventListener("input", ({ target }) => {
+  debounceGet(target.value);
+});
+
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  debounceGet(searchInput.value);
 });
 
 window.addEventListener("load", () => {
   setTheme();
   setIcons();
-  api.get("all").then((data) => {
-    data.forEach((item) => {
-      countryUI.appendOnDisplay(parseCountrySchema(item));
-    });
-  });
+
+  api.get("all").then(populateDisplay);
 });
