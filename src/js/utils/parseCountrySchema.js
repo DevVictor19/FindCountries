45a3ec6e7161f2b8da66849c6514ce0af1339b9d
Antonio Victor <Antoniovictor12@live.com>
2022-12-09 10:42:03 -0300

    feat: adicionar função de formatação do schema

---
+++ src/js/utils/parseCountrySchema.js
@@ -0,0 +1,15 @@
+export function parseCountrySchema(data) {
+  return {
+    name: data.name.official,
+    region: data.region,
+    population: data.population,
+    subregion: data.subregion,
+    capital: data.capital,
+    nativeName: data.name.nativeName,
+    currencies: data.currencies,
+    languages: data.languages,
+    tld: data.tld,
+    borders: data.borders,
+    flagUrl: data.flags.png,
+  };
+}
